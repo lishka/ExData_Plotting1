3db4ccad25064ec53897bb42073c592e50264ae2
lishka <lishka@users.noreply.github.com>
2020-03-25 16:11:40 +0100

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,8 @@
+table<-read.csv("household_power_consumption.txt", sep=";", dec = ".", na.strings = "?")
+Feb1<-subset(table, table$Date == "1/2/2007")
+Feb2<-subset(table, table$Date == "2/2/2007")
+Feb12<-rbind(Feb1, Feb2)
+hist(Feb12$Global_active_power, col = "red", breaks=15, xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+png("plot1.png")
+hist(Feb12$Global_active_power, col = "red", breaks=15, xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
